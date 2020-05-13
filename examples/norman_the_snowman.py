3edbf6796dbe9a3a8c1f7bf2e616901fe4aade47
masashi yoshikawa <earnest.marshi@gmail.com>
2020-05-13 12:20:36 +0900

    update example

--- examples/norman_the_snowman.py
+++ examples/norman_the_snowman.py
@@ -16,4 +16,4 @@ player['h'] = '2  1          1 5           5 432  1          1 3       4 3 2 1 '
 sp = player.sample_player
 sp['kick'] = 'x x x x '
 sp['hat'] = '- - - - '
-sp['snare'] = '  o   o '
+sp['snare'] = '   ooo  '
