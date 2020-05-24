fef06ba4d0c368df2983200715ff7eff9546dc8f
masashi yoshikawa <earnest.marshi@gmail.com>
2020-05-24 22:02:45 +0900

    update

--- toid/__init__.py
+++ toid/__init__.py
@@ -12,6 +12,8 @@ Phrase = toid.data.Phrase  # NOQA
 Track = toid.data.Track  # NOQA
 Pitch = toid.data.Pitch  # NOQA
 Beat = toid.data.Beat  # NOQA
+PitchInterval = toid.data.PitchInterval  # NOQA
+PitchInOctave = toid.data.PitchInOctave  # NOQA
 
 example_sf2_path = str(
     pathlib.Path(os.path.dirname(__file__)) / 'sample-resource' / 'sf2' / 'sf2.toml'
