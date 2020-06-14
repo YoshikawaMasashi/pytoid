a1bba4ed5ae9ea557c636ab1e2e7f6b6b9fb1d03
masashi yoshikawa <earnest.marshi@gmail.com>
2020-06-14 18:53:41 +0900

    wave file outputter

--- src/outputters/mod.rs
+++ src/outputters/mod.rs
@@ -1,13 +1,16 @@
 mod portaudio_outputter;
+mod wave_file_outputter;
 
 use pyo3::prelude::{pymodule, PyModule, PyResult, Python};
 use pyo3::wrap_pymodule;
 
 use portaudio_outputter::PortAudioOutputter;
+use wave_file_outputter::WaveFileOutputter;
 
 #[pymodule]
 fn outputters(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<PortAudioOutputter>()?;
+    m.add_class::<WaveFileOutputter>()?;
 
     Ok(())
 }
